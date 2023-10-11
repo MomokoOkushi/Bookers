84dee4cae9dc3458a792bba719606630448691ab
EC2 Default User <ec2-user@ip-172-31-34-105.ap-northeast-1.compute.internal>
2023-10-11 10:25:55 +0000

    form_with

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'books' => 'books#new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show'
+  get 'edit' => 'books#edit'
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
