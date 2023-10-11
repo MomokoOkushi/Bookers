3444bb1dcda3af137059dbf18902aec7c41606a8
EC2 Default User <ec2-user@ip-172-31-34-105.ap-northeast-1.compute.internal>
2023-10-11 07:04:35 +0000

    routing

--- db/migrate/20231010181600_create_books.rb
+++ db/migrate/20231010181600_create_books.rb
@@ -1,7 +1,11 @@
+#ここにテーブル名、データ型、カラム
+#その後、ターミナルで rails db:
+#schemaファイルが作成される。テーブル完成したよ
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-
+      t.string :title
+      t.string :body
       t.timestamps
     end
   end
